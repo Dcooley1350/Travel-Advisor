efb39fe4b56ac268a51cbdf710069e09447d7646
Devin Cooley <dcooley1350@gmail.com>
2019-06-11 13:21:49 -0700

    add script to collect user input and define variables.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function()  {
+  $(form#survey).submit(function()  {
+    var userName = $("firstName").val();
+    var userGender = $("gender").val();
+    var userCityOrRural = $("cityORrural").val();
+    var userClothing = $("clothingType").val();
+    var userWater = $("input:radio[name=water-type]:checked").val();
+
+    if (userGender === "Male?") {
+      var prefix = "Mr.";
+    }
+    if (userGender === "Female?") {
+      var prefix = "Miss";
+    }
+    if (userGender === "I prefer not to identify. We are all people!") {
+      var prefix = "Non Demoninational Person"
+    }
+  })
+})
