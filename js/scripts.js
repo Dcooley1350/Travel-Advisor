69d199445cfb4316823a2636c696bb0425876880
Devin Cooley <dcooley1350@gmail.com>
2019-06-11 18:25:51 -0700

    add effect and jquery to fade out previously suggested locations when a new form is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function()  {
   $("#survey form").submit(function(event)  {
     event.preventDefault();
+
     var userName = $("input#firstName").val();
     var userGender = $("#gender").val();
     var userCityOrRural = $("#cityORrural").val();
@@ -27,16 +28,22 @@ $(document).ready(function()  {
 
 
     if (userCityOrRural === "3" && userWater === "ocean") {
+      $("#CityRiver, #CityLake, #RuralOcean, #RuralRiver, #RuralLake").fadeOut();
       $("#CityOcean").fadeIn();
     } else if (userCityOrRural === "3" && userWater === "river" ) {
+      $("#CityOcean, #CityLake, #RuralOcean, #RuralRiver, #RuralLake").fadeOut();
       $("#CityRiver").fadeIn();
     } else if (userCityOrRural === "3" && userWater === "lake") {
+      $("#CityRiver, #CityOcean, #RuralOcean, #RuralRiver, #RuralLake").fadeOut();
       $("#CityLake").fadeIn();
     } else if (userCityOrRural === "4" && userWater === "ocean") {
+      $("#CityRiver, #CityLake, #CityOcean, #RuralRiver, #RuralLake").fadeOut();
       $("#RuralOcean").fadeIn();
     } else if (userCityOrRural === "4" && userWater === "river") {
+      $("#CityRiver, #CityLake, #RuralOcean, #CityOcean, #RuralLake").fadeOut();
       $("#RuralRiver").fadeIn();
     } else if (userCityOrRural === "4" && userWater === "lake") {
+      $("#CityRiver, #CityLake, #RuralOcean, #RuralOcean, #RuralRiver, #CityOcean").fadeOut();
       $("#RuralLake").fadeIn();
     } 
   })
